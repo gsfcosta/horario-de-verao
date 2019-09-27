e9315f1a0ce3274abed41b3ebae4e85e53ea135f
Gabriel Silva <42187636+gsfcosta@users.noreply.github.com>
2019-09-27 15:51:26 -0300

    Update linux_connect.sh

--- scripts/linux/linux_connect.sh
+++ scripts/linux/linux_connect.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 clear
-for ip in $(cat /etc/ansible/scripts/linux/acessos.txt); do
-	nc -zvw 1 $ip 22
-	if (( $? == 0 )); then
-		echo "$ip > SUCCESS" >> /etc/ansible/scripts/linux/linux.log
-		echo "$ip > SUCCESS"
-	else
-		echo "$ip > FAILED" >> /etc/ansible/scripts/linux/linux.log
+i=0
+for ip in $(cat /ansible/scripts/linux/acessos.txt); do
+        nc -zvw 1 $ip 22
+        if (( $? == 0 )); then
+                echo "$ip > SUCCESS" >> /ansible/scripts/linux/list.txt
+                echo "$ip > SUCCESS"
+        #       echo "[linux$i]" >> /etc/ansible/hosts
+        #       echo $ip >> /etc/ansible/hosts; echo
+                let i=(i+1)
+        else
+                echo "$ip > FAILED" >> /ansible/scripts/linux/list.txt
                 echo "$ip > FAILED"
-	fi
-	echo; echo "-------------------------------------------------"; echo;
+        fi
+        echo; echo "-------------------------------------------------"; echo;
 done
+#bash /etc/ansible/scripts/linux/sshcopy.sh
